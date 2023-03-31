8a6f9acfac49858298a2a2de0160d9acf872ae4d
isadora <isadora.bayma@gmail.com>
2023-03-31 15:02:41 -0300

    css screen versions

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -5,9 +5,9 @@ import { EnvelopeOpen, GithubLogo, LinkedinLogo } from "phosphor-react";
 export function CoffeeCard() {
   return (
     <div>
-      <div className="pt-[40rem] pr-3 flex justify-end">
-        <div className="pt-28 pr-[40px] absolute">
-          <div className="transition-all ease-in delay-150 duration-1000 bg-teal-900 h-[9rem] w-[33rem] hover:w-[48rem] rounded-md place-self-end pl-8 flex flex-row space-x-6">
+      <div className="flex lg:pt-[40rem]">
+        <div className="absolute transition-all duration-700 -left-10 sm:left-10 md:left-24 xl:left-40 hover:-left-[20rem] md:hover:-left-[10rem] pt-8 lg:pt-28 lg:pr-[40px]">
+          <div className="transition-all ease-in delay-150 duration-1000 bg-teal-900 h-[9rem] w-[33rem] lg:hover:w-[48rem] rounded-md place-self-end pl-8 flex flex-row space-x-6">
             <div className="my-auto flex flex-row space-x-5">
               <div>
                 <h1 className="text-4xl text-white">Isadora Bayma</h1>
@@ -33,7 +33,7 @@ export function CoffeeCard() {
         <img
           src={coffee}
           alt=""
-          className="rotate-[38deg] hover:rotate-45 h-72 place-self-center drop-shadow-2xl"
+          className="invisible lg:visible rotate-[38deg] hover:rotate-45 h-72 drop-shadow-2xl ml-[20rem] xl:ml-[24rem] sxl:ml-[30rem]"
         />
       </div>
     </div>
