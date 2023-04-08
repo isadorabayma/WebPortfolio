3c58a3d48494baba77c54837a79ff8598a40f3df
isadora <isadora.bayma@gmail.com>
2023-04-08 13:54:00 -0300

    css bg image replace

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,11 @@ function App() {
           {/* <img
           src="https://cdn.pixabay.com/photo/2015/12/03/08/50/paper-1074131_960_720.jpg"
           className="w-2/3 h-[50rem] mt-16 object-cover absolute ring-2 ring-gray-900/5 z-10 drop-shadow-2xl rounded-md"
-          /> */}
-          <div className="absolute top-16 right-36 2xl:left-10
-           bg-[url('/Images/concrete.jpg')] w-[35rem] h-[50rem] object-cover ring-2 ring-gray-900/10 z-10 drop-shadow-2xl rounded-md"></div>
+          /> 
+          
+          bg-[url('/Images/concrete.jpg')]*/}
+          <div className="absolute top-16 right-36 2xl:left-10 
+           bg-stone-700 w-[35rem] h-[50rem] object-cover ring-2 ring-gray-900/10 z-10 drop-shadow-2xl rounded-md"></div>
           <Cv color={"bg-stone-200"} z={1} rotate={"-rotate-2"} />
           <Skills color={"bg-stone-200"} z={3} rotate={"-rotate-3"} />
           <Cv color={"bg-stone-200"} z={2} rotate={"rotate-3"} />
