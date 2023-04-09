da229015e96a8ee3ce89bad12bc1ef7c14d8bb55
isadora <isadora.bayma@gmail.com>
2023-04-09 00:17:43 -0300

    fix: component sheets label first try

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ function App() {
       <Header />
       <div className="w-full grid grid-cols-2">
         <div className="relative">
-          <div className="absolute top-16 right-36 2xl:left-10 
+          <div className="absolute top-16 right-44 2xl:left-10 
            bg-stone-700 w-[35rem] h-[50rem] object-cover ring-2 ring-gray-900/10 z-10 drop-shadow-2xl rounded-md"></div>
-          <Sheet margin={"mt-[22rem]"} z={1} content={"About"} rotate={"-rotate-2"} />
-          <Sheet margin={"mt-[28rem]"} z={2} content={"Skills"} rotate={"rotate-3"} />
-          <Sheet margin={"mt-[34rem]"} z={3} content={"Experience"} rotate={"-rotate-3"} />
+          <Sheet margin={"mt-[20rem]"} z={1} content={"About"} rotate={"-rotate-3"} />
+          <Sheet margin={"mt-[28rem]"} z={3} content={"Skills"} rotate={"-rotate-2"} />
+          <Sheet margin={"mt-[36rem]"} z={2} content={"Experience"} rotate={"rotate-1"} />
         </div>
         <div className="relative">
           <Tablet />
--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -10,14 +10,14 @@ interface SheetProps {
 }
 
 export function Sheet(props: SheetProps) {
-  // const marginLabel = 40 - Number(props.z) * 6 
-  // const margin = `mt-[${marginLabel}rem]`
-  // console.log(margin);
+  const marginLabel = 40 - Number(props.z) * 8 
+  const margin = `mt-[${marginLabel}rem]`
+  console.log(margin);
   
   return (
     <>
       <div
-        className={`transition-all ease-in delay-150 duration-1000 absolute right-12 2xl:left-10 z-${props.z} hover:-right-[19rem] ${props.rotate} hover:rotate-0 m-16 hover:m-4 hover:z-30 flex flex-row `}
+        className={`transition-all ease-in delay-150 duration-1000 absolute right-12 2xl:left-10 z-${props.z} hover:-right-[19rem] ${props.rotate} hover:rotate-0 m-16 hover:m-4 hover:z-30 flex flex-row`}
       >
         <div className="bg-stone-200 ring-1 ring-gray-900/10 w-[35rem] h-[50rem] drop-shadow-xl hover:drop-shadow-2xl">
           <div>
@@ -26,7 +26,7 @@ export function Sheet(props: SheetProps) {
             {"Experience" === props.content && <Experience/>}
           </div>
         </div>
-        <div className={`bg-stone-200 ring-1 ring-gray-900/10 h-32 w-10 drop-shadow-xl hover:drop-shadow-2xl ${props.margin}`}>
+        <div className={`bg-stone-200 h-32 w-10 drop-shadow-xl hover:drop-shadow-2xl ${props.margin} rounded-md`}>
           <div className="-rotate-90 mt-20">
             { props.content }
           </div>
