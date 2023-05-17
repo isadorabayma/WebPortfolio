4f86ef4034486dff05a9c8aeab5aa800ddbe8e33
isadora <isadora.bayma@gmail.com>
2023-05-17 03:16:00 -0300

    style: shadows file

--- src/components/Files.tsx
+++ src/components/Files.tsx
@@ -11,13 +11,16 @@ export function Files({sectionInfo, handleSection}: FileProps) {
         id="file-cover"
         className="absolute 
         top-[26rem] xl:top-[20rem] -left-[21rem] xl:-left-16 -rotate-[27deg]
-         w-[45rem] h-[60rem] ring-2 ring-gray-900/10 z-10 drop-shadow-2xl rounded-r-md"
+         w-[45rem] h-[60rem] ring-2 ring-gray-900/10 z-10 drop-shadow-2xl rounded-r-md shadow-3xl"
       >
         <img
           src={CardTexture}
           alt="Foto de perfil da Isadora"
           className="absolute h-full w-full object-cover rounded-r-md"
         />
+      </div>
+      <div id="shadow-file" className="absolute z-[0.5] top-[26rem] xl:top-[22rem] -left-[19rem] xl:-left-10 -rotate-[27deg]
+         w-[45rem] h-[60rem] bg-black/70 blur-2xl">
       </div>
       <div id="sheets">
         {sectionInfo.map(({name, z, rotate, right}, index)=>(
