ea55cedce62d5883b351e514a13d8ba165fd0fea
isadora <isadora.bayma@gmail.com>
2023-05-17 13:37:42 -0300

    feat: projects wrap version

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,9 +6,9 @@ export function ProjectCard({project}: ProjectCardProps) {
   return (
     <div className="bg-white overflow-hidden rounded-2xl my-5 drop-shadow-xl">
       <img src={ image } alt={ alt } />
-      <div className="px-3">
-        <h2 className="my-2 font-bold md:text-lg">{ name }</h2>
-        <p className="line-clamp-6">{ description } </p>
+      <div className="px-3 md:px-7">
+        <h2 className="mt-8 mb-3 font-bold md:text-xl">{ name }</h2>
+        <p className="text-justify">{ description } </p>
         <div className="flex flex-row justify-center mt-5 space-x-5 font-bold">
           { lenguages.map((lenguage, index) => (
             <>
@@ -17,18 +17,14 @@ export function ProjectCard({project}: ProjectCardProps) {
             </>
           ))}
         </div>
-        <div className="flex flex-row justify-start mt-2 mb-5">
-          <div className="flex flex-row justify-start pr-6 truncate">
-            { libraries.map((library, index) => (
-                index > 0 ? (
-                  <div key={library}>{`, ${library}`}</div>
-                ) : (
-                  <div key={library}>{ library }</div> 
-                )
-            ))}
-          </div>
-          { libraries.length > 0 && <div>...</div> }
-
+        <div className="flex flex-wrap justify-start space-x-1 mt-2 mb-5">
+          { libraries.map((library, index) => (
+              index !== (libraries.length - 1) ? (
+                <div key={library}>{`${library},`}</div>
+              ) : (
+                <div key={library}>{`${library}.`}</div> 
+              )
+          ))}
         </div>
         <div className="flex flex-row justify-around p-1 py-4 font-bold border-t-2 border-stone-800">
           <a href={demostration} aria-label="Demonstração">Demonstração</a>
