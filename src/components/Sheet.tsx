70009fec433a3967e7ec21cac2fad75b4daf06ff
isadora <isadora.bayma@gmail.com>
2023-05-23 19:30:13 -0300

    fix-position sheet and skills

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -12,7 +12,7 @@ export function Sheet({name, z, rotate, right, handleSection, index}: SheetProps
     <div id={`sheet-${name}`} className={`absolute ${z} ${rotate} ${right} drop-shadow-xl bg-stone-200 ring-1 ring-gray-900/10`}>
       <div className="h-[53rem]">
         <div className="flex flex-row justify-between mt-10 mx-10">
-          <h1 id="title-sheet-section" className="text-5xl text-stone-800 font-serif font-semibold">{name}</h1>
+          <h1 id="title-sheet-section" className="text-4xl text-stone-800 font-serif font-semibold">{name}</h1>
           <div className="flex flex-row mt-3 mx-0 sm:mx-3 space-x-3">
             {name === sectionNames[0] &&
             <button onClick={() => handleSection(name)}>
@@ -36,7 +36,7 @@ export function Sheet({name, z, rotate, right, handleSection, index}: SheetProps
             }
           </div>
         </div>
-        <div id="content-sheet-section" className="relative border-y-2 border-stone-800 mx-10 h-[47.5rem]">
+        <div id="content-sheet-section" className="relative border-y-2 border-stone-800  mx-5 sm:mx-10 h-[47.5rem]">
           {"About" === name && <About/>}
           {"Skills" === name && <Skills/>}
           {"Experience" === name && <Experience/>}
--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,4 +1,4 @@
-import { SkillType } from '../skillsInfo';
+import { SkillType } from "../skillsInfo";
 
 interface SkillsSectionProps {
   skills: SkillType[];
@@ -7,19 +7,21 @@ interface SkillsSectionProps {
 
 export function SkillsSection({ skills, title }: SkillsSectionProps) {
   return (
-      <div className="pt-8">
-        <h2 className="text-3xl font-serif font-semibold pl-4 border-b-2 border-stone-300 sm:border-b-0 ">{title}</h2>
-        <div className="flex flex-wrap sm:border-l-2 sm:border-stone-800 sm:ml-5 sm:px-3 sm:pl-4">
-          {skills.map(([Icon, Name]: SkillType) => (
-            <div
-              key={Name}
-              className="pt-2 mx-1 flex flex-col items-center w-16 sm:w-20 " 
-            >
-              <Icon size={50} />
-              <h2 className="pt-1 text-sm sm:text-base text-center">{Name}</h2>
-            </div>
-          ))}
-        </div>
+    <div className="pt-8">
+      <h2 className="text-2xl sm:text-3xl font-serif font-semibold pl-4 border-b-2 border-stone-300 sm:border-b-0 ">
+        {title}
+      </h2>
+      <div className="flex flex-wrap space-x-2 sm:border-l-2 sm:border-stone-800 sm:ml-5 sm:px-3 sm:pl-4">
+        {skills.map(([Icon, Name]: SkillType) => (
+          <div
+            key={Name}
+            className="pt-2 flex flex-col items-center w-16 sm:w-20 "
+          >
+            <Icon size={50} />
+            <h2 className="pt-1 text-sm sm:text-base text-center">{Name}</h2>
+          </div>
+        ))}
       </div>
+    </div>
   );
 }
