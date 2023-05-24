2b40d86feadf922a3da47c9a18b0ef3204527b65
isadora <isadora.bayma@gmail.com>
2023-05-24 19:12:38 -0300

    style-refine experience sheet

--- src/components/ExpSection.tsx
+++ src/components/ExpSection.tsx
@@ -12,16 +12,17 @@ export function ExpSection({ data }: ExpSectionProps) {
       { data.map((exp, i) => {
         const directionFull = exp.category !== "Experiencia Proficional" ? "text-left" : "text-right";
         const directionBar = exp.category === "Experiencia Proficional" ? "flex-row" : "flex-row-reverse";
-        const margin = i <= 4 && "-my-5 sm:-my-4"
+        const rotatePoint = exp.category !== "Experiencia Proficional" ? "rotate-[135deg]" : "-rotate-45";
+        const margin = i <= 5 && "-mt-10 sm:-my-4"
 
         return(
           <div key={exp.name} className={`flex ${directionBar} ${margin} justify-center relative`}>
-            <div id="exp-info" className="w-1/2 py-2 sm:py-4" >
-              <div className={directionFull}>{exp.name}</div>
-              <div className={directionFull}>{exp.place}</div>
-              <div className={directionFull}>{`${exp.startYear} - ${exp.endYear}`}</div>
+            <div id="exp-info" className="w-1/2 py-5" >
+              <div className={`font-bold  text-teal-900 ${directionFull}`}>{exp.name}</div>
+              <div className={`font-bold ${directionFull}`}>{exp.place}</div>
+              <div className={`font-bol ${directionFull}`}>{`${exp.startYear} - ${exp.endYear}`}</div>
             </div>
-            <div id="duration-time-line" className="top-3 w-4 h-4 mt-6 mx-2 bg-teal-900 rounded-full"></div>
+            <div id="duration-time-line" className={`w-5 h-5 mt-[1.4rem] mx-3 bg-teal-700/60 rounded-b-full rounded-tr-full ${rotatePoint}`}></div>
             <div className="w-1/2"></div>
           </div>
         )})}
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,9 +5,9 @@ export function Experience() {
   const expSort = experiences.sort((a, b) => Number(b.endYear) - Number(a.endYear));
 
   return(
-    <div className="h-full mt-12 relative">
+    <div className="h-full mt-16 relative">
       <ExpSection data={expSort}/>
-      <div className="absolute -top-2 h-[40rem] w-1/2 border-r-2 border-stone-800 "></div>
+      <div className="absolute -top-8 h-[42rem] w-1/2 border-r-2 border-stone-800 "></div>
     </div>
   );
 }
